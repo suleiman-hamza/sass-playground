8aaa3eb6cbac161b09e18edfd071d36c326562f3
suleiman hamza <hamzasuleiman530@gmail.com>
2025-07-11 04:58:26 +0100

    [hotfix]: replaced deineNuxtPlugin with defineNitroPlugin

--- server/plugins/test.ts
+++ server/plugins/test.ts
@@ -1,6 +1,4 @@
-import { defineNuxtPlugin } from "nuxt/app";
-
-export default defineNuxtPlugin((nitroApp) => {
+export default defineNitroPlugin((nitroApp) => {
     // This plugin is used to test the server-side functionality
     // It can be used to test server-side logic, database connections, etc.
     console.log('Nitro Plugin', nitroApp);
