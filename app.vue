f9beee865202ebc139489412fd600faf3264a20d
suleiman hamza <hamzasuleiman530@gmail.com>
2025-07-11 04:09:42 +0100

    [feat]: add initial app structure with fetch functionality and server route

--- app.vue
+++ app.vue
@@ -1,5 +1,13 @@
 <template>
   <div>
     <h2>Hello Nuxt</h2>
+    <button>Fetch</button>
+    <p v-if="response">{{ response }}</p>
   </div>
 </template>
+
+<script setup lang="ts">
+  const config = useRuntimeConfig();
+  // console.log('API Secret:', config.public.apiBase);
+  const { data:response } = useAsyncData('fetchData', () => $fetch('/hello'));
+</script>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,11 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: '2025-05-15',
-  devtools: { enabled: true }
+  devtools: { enabled: true },
+  runtimeConfig: {
+    apiSecret: '123445555',
+    public: {
+      apiBase: 'a-publick-api-key'
+    }
+  }
 })
